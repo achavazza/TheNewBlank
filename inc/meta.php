6cf798dc4c9acf69fe3235c4150712f035c5233a
achavazza <alejandrochavazza@gmail.com>
2019-06-08 11:54:41 -0300

    first

--- inc/meta.php
+++ inc/meta.php
@@ -1,6 +1,9 @@
 <div class="meta">
-	<?php the_time(get_option('date_format')) ?>
 	<em>Posted on:</em> <?php the_time('F jS, Y') ?>
 	<em>by</em> <?php the_author() ?>
-	<?php comments_popup_link('No Comments', '1 Comment', '% Comments', 'comments-link', ''); ?>
+	<?php the_time(get_option('date_format')) ?>
+	<hr />
+	Posted in <?php the_category(', ') ?> |
+	<?php the_tags('Tags: ', ', ', '<br />'); ?>
+	<?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
 </div>
